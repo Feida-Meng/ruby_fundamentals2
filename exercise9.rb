3c7f77e9303b1e0bf3f49b2cb9d1243d22696b30
codonculous <feidameng01@gmail.com>
2017-03-28 15:23:55 -0400

    excersie9 finished

--- exercise9.rb
+++ exercise9.rb
@@ -1,5 +1,20 @@
 grocery_list = ["noodle","eggs","orange","apple"]
-grocery_list << "rice"
+grocery_list << "rice" # step1
+
+puts "Number of items on the grocery list is #{grocery_list.count}." #step 2
+
+if grocery_list.include?("banana") #step3
+  puts "You need to pick up bananas"
+else
+  puts "You don't need to pick up bananas today"
+end
+
+puts grocery_list[1] #step4
+
+grocery_list = grocery_list.sort #step5
+
+grocery_list.pop #step 6
+
 def show_list(array)
   array.each do |item|
   puts "* #{item}"
