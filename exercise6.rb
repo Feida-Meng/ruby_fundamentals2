550511659d842bd80b4f24666af7d1742ef45f44
codonculous <feidameng01@gmail.com>
2017-03-28 14:36:07 -0400

    exercise6 finished

--- exercise6.rb
+++ exercise6.rb
@@ -1,1 +1,12 @@
 
+puts "Please enter the temperature in Fahrenheit"
+temp_fah = gets.chomp
+
+
+def convert_fah_to_c(fah)
+
+  return (fah.to_i - 32) * 5 / 9
+
+end
+
+puts "The #{temp_fah} F is equal to #{convert_fah_to_c(temp_fah)} C"
