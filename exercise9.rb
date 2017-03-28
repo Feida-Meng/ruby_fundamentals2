9e2f2abcc8cb7536542febcc18194e03cba96aaa
codonculous <feidameng01@gmail.com>
2017-03-28 15:14:09 -0400

    Create method to output listed item with * in the front and add rice to the list

--- exercise9.rb
+++ exercise9.rb
@@ -1,5 +1,9 @@
 grocery_list = ["noodle","eggs","orange","apple"]
-
-grocery_list.each do |item|
+grocery_list << "rice"
+def show_list(array)
+  array.each do |item|
   puts "* #{item}"
+  end
 end
+
+show_list(grocery_list)
