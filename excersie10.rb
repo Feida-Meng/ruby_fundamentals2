3477bd317c1f4d769d3a7106a90da859f8318311
codonculous <feidameng01@gmail.com>
2017-03-28 15:35:56 -0400

    create the method to show the size of each cohort

--- excersie10.rb
+++ excersie10.rb
@@ -1,9 +1,20 @@
 #step1
-student = {
+students = {
   cohort1:34,
   cohort2:42,
   cohort3:22
 
 }
 
-puts student
+
+#step 2
+def cohort_size(hash)
+
+  hash.each do |value,key|
+    puts "#{value}: #{key} students"
+  end
+
+end
+
+
+cohort_size(students)
