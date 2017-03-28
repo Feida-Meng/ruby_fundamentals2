566834cbfd5b62dfeab1f957668fd7e19836be67
codonculous <feidameng01@gmail.com>
2017-03-28 16:11:26 -0400

    Create method to find which dog is absent

--- excersie11.rb
+++ excersie11.rb
@@ -1,6 +1,27 @@
 my_dogs = [
-  { name:"Ralph", position: 5}
-  { name:"Cindy", position: 8}
+  { name:"Ralph", position: 5},
+  { name:"Cindy", position: 12},
   { name:"Jade", position: 11}
 
 ]
+
+#step 1
+def get_absent_dogs(dogs)
+  absent_dogs = []
+
+  dogs.each do |dog|
+
+
+
+      if (dog[:position] > 10)
+      absent_dogs.push(dog[:name])
+      end
+
+
+  end
+
+  return absent_dogs
+
+end
+
+puts get_absent_dogs(my_dogs)
